f10e2119c4b41d456d7eeb96ecb464965eb69ad4
Finble <elainefinn@hotmail.com>
2016-02-17 15:40:17 +0000

    Temperature method added, passing both feature and unit tests

--- jasmine-standalone-2.4.1 (1)/spec/ThermostatSpec.js
+++ jasmine-standalone-2.4.1 (1)/spec/ThermostatSpec.js
@@ -6,4 +6,16 @@ describe('Thermostat', function(){
 		var thermostat = new Thermostat();
 		expect(thermostat).toBeDefined();
 	});
+
+	it('calls a method that starts at 20 degrees', function(){
+		var thermostat = new Thermostat();
+		thermostat.temperature();
+		expect(thermostat.temperature).toEqual(20);
+	});
 });
+
+	// it('Temperature method is set up', function(){
+	// 	var thermostat = new Thermostat();
+	// 	thermostat.temperature();
+	// 	expect(thermostat.temperature).toBeDefined();
+	// });
--- jasmine-standalone-2.4.1 (1)/src/Thermostat.js
+++ jasmine-standalone-2.4.1 (1)/src/Thermostat.js
@@ -1,3 +1,7 @@
 'use strict';
 
 function Thermostat() {};
+
+Thermostat.prototype.temperature = function(){
+
+};
