9383982ace7c676a2298f5dd123cffe6f4221bd2
Finble <elainefinn@hotmail.com>
2016-02-17 15:30:52 +0000

    Thermostat object set up, with feature and unit tests passing

---
+++ jasmine-standalone-2.4.1 (1)/spec/ThermostatSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Thermostat', function(){
+	
+	it('Thermostat is defined', function(){
+		var thermostat = new Thermostat();
+		expect(thermostat).toBeDefined();
+	});
+});
